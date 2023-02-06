74fc34caf0269506e1974e6cc7f536712f75e143
EugeneVakarchuk <eugenevakarchuk@gmail.com>
2023-02-06 20:08:22 +0200

    equal function refactored

--- src/mathLogic/equal.js
+++ src/mathLogic/equal.js
@@ -27,90 +27,86 @@ const equal = (
     setNumsDef(true);
   }
 
+  const firstIntMath = () => {
+    setMemoryNum(output);
+    setNumsDef(true);
+    setIsFloatNum(false);
+  }
+
+  const intEqual = (mathFunc, numDef) => {
+    switch (true) {
+      case (mathFunc === 'plus' && numDef === false):
+        firstIntMath()
+        setInputNum(plus(output, memoryNum));
+        break;
+      case (mathFunc === 'minus' && numDef === false):
+        firstIntMath()
+        setInputNum(minus(output, memoryNum));
+        break;
+      case (mathFunc === 'divide' && numDef === false):
+        firstIntMath()
+        setInputNum(divide(output, memoryNum));
+        break;
+      case (mathFunc === 'multiply' && numDef === false):
+        firstIntMath()
+        setInputNum(multiply(output, memoryNum));
+        break;
+      case (mathFunc === 'plus' && numDef === true):
+        setInputNum(plus(output, memoryNum));
+        break;
+      case (mathFunc === 'minus' && numDef === true):
+        setInputNum(minus(memoryNum, output));
+        break;
+      case (mathFunc === 'divide' && numDef === true):
+        setInputNum(divide(memoryNum, output));
+        break;
+      case (mathFunc === 'multiply' && numDef === true):
+        setInputNum(multiply(output, memoryNum));
+        break;
+      default:
+    }
+  }
+
+
+  const floatEqual = (mathFunc, numDef) => {
+    switch (true) {
+      case (mathFunc === 'plus' && numDef === false):
+        firstMath()
+        setInputNum(plusFloat(output, memoryNum, intergerPart));
+        break;
+      case (mathFunc === 'minus' && numDef === false):
+        firstMath()
+        setInputNum(minusFloat(output, memoryNum, intergerPart));
+        break;
+      case (mathFunc === 'divide' && numDef === false):
+        firstMath()
+        setInputNum(divideFloat(output, memoryNum, intergerPart));
+        break;
+      case (mathFunc === 'multiply' && numDef === false):
+        firstMath()
+        setInputNum(multiplyFloat(output, memoryNum, intergerPart));
+        break;
+      case (mathFunc === 'plus' && numDef === true):
+        setInputNum(plusFloat(output, memoryNum, intergerPart));
+        break;
+      case (mathFunc === 'minus' && numDef === true):
+        setInputNum(minusFloat(memoryNum, output, intergerPart));
+        break;
+      case (mathFunc === 'divide' && numDef === true):
+        setInputNum(divideFloat(memoryNum, output, intergerPart));
+        break;
+      case (mathFunc === 'multiply' && numDef === true):
+        setInputNum(multiplyFloat(output, memoryNum, intergerPart));
+        break;
+      default:
+    }
+  }
+
+
   if (!isFloatNum) {
-    if (numsDef === false) {
-      switch (true) {
-        case (mathFunc === 'plus'):
-          firstMath()
-          setInputNum(plus(output, memoryNum));
-          setIsFloatNum(false)
-          break;
-        case (mathFunc === 'minus'):
-          firstMath()
-          setInputNum(minus(output, memoryNum));
-          setIsFloatNum(false)
-          break;
-        case (mathFunc === 'divide'):
-          firstMath()
-          setInputNum(divide(output, memoryNum));
-          setIsFloatNum(false)
-          break;
-        case (mathFunc === 'multiply'):
-          firstMath()
-          setInputNum(multiply(output, memoryNum));
-          setIsFloatNum(false)
-          break;
-        default:
-      }
-    } else {
-      switch (true) {
-        case (mathFunc === 'plus'):
-          setInputNum(plus(output, memoryNum));
-          setIsFloatNum(false)
-          break;
-        case (mathFunc === 'minus'):
-          setInputNum(minus(memoryNum, output));
-          setIsFloatNum(false)
-          break;
-        case (mathFunc === 'divide'):
-          setInputNum(divide(memoryNum, output));
-          setIsFloatNum(false)
-          break;
-        case (mathFunc === 'multiply'):
-          setInputNum(multiply(output, memoryNum));
-          setIsFloatNum(false)
-          break;
-        default:
-      }
-    }
+    intEqual(mathFunc, numsDef);
   } else {
-    if (numsDef === false) {
-      switch (true) {
-        case (mathFunc === 'plus'):
-          firstMath()
-          setInputNum(plusFloat(output, memoryNum, intergerPart));
-          break;
-        case (mathFunc === 'minus'):
-          firstMath()
-          setInputNum(minusFloat(output, memoryNum, intergerPart));
-          break;
-        case (mathFunc === 'divide'):
-          firstMath()
-          setInputNum(divideFloat(output, memoryNum, intergerPart));
-          break;
-        case (mathFunc === 'multiply'):
-          firstMath()
-          setInputNum(multiplyFloat(output, memoryNum, intergerPart));
-          break;
-        default:
-      }
-    } else {
-      switch (true) {
-        case (mathFunc === 'plus'):
-          setInputNum(plusFloat(output, memoryNum, intergerPart));
-          break;
-        case (mathFunc === 'minus'):
-          setInputNum(minusFloat(memoryNum, output, intergerPart));
-          break;
-        case (mathFunc === 'divide'):
-          setInputNum(divideFloat(memoryNum, output, intergerPart));
-          break;
-        case (mathFunc === 'multiply'):
-          setInputNum(multiplyFloat(output, memoryNum, intergerPart));
-          break;
-        default:
-      }
-    }
+    floatEqual(mathFunc, numsDef);
   }
 }
 
